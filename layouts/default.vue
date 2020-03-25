534156e38fcb5af2c0eadd5d40edefe84571891b
aniaska4 <aniaska4@wp.pl>
2020-03-25 14:19:16 +0100

    initialize project

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+  
+<template>
+  <div>
+    <Nav />
+    <Information />
+    <Actuality />
+    <Team />
+    <Contact />
+  </div>
+</template>
+<script>
+import Nav from './partials/Nav'
+import Information from './partials/Information'
+import Actuality from './partials/Actuality'
+import Team from './partials/Team'
+import Contact from './partials/Contact'
+export default {
+  components: {
+    Nav,
+    Information,
+    Actuality,
+    Team,
+    Contact
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
+
+<style>
+
+</style>
