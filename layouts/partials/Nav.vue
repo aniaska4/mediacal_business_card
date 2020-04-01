6c7ae7fb6aea8f3b9beaa745c78535bffdbe8d9c
aniaska4 <aniaska4@wp.pl>
2020-04-01 09:43:46 +0200

    RVD nav

--- layouts/partials/Nav.vue
+++ layouts/partials/Nav.vue
@@ -8,10 +8,10 @@
                     h1 Zespół Poradni Rehabilitacyjnej Powiatu Warszawskiego Zachodniego
                     .nav_places
                         span(v-for="place in places") {{ place }}
+            .nav_places_mobile
+                span(v-for="place in places") {{ place }}
             .main_nav
                 ul
-                    li 
-                        i.fas.fa-home
                     li.scroll(v-for="nav in navBar") {{ nav }}             
 </template>
 <script>
@@ -20,7 +20,7 @@ export default {
     data(){
         return{
             places: ["Błonie", "Ożarów Mazowiecki", "Dziekanów Leśny", "Blizne Jasińskiego"],
-            navBar: ["Nasza poradnia", "aktualności", "oferta", "do pobrania", "blog", "kontakt" ]
+            navBar: ["Poradnia", "Aktualności", "Oferta", "Do pobrania", "Blog", "Kontakt" ]
         }
     }
 }
@@ -28,15 +28,14 @@ export default {
 <style lang="scss" scoped>
 @import "styles/scss/variables.scss";
 @import "styles/scss/reset.scss";
+@import "styles/scss/rwd.scss";
 .nav_title {
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
     margin-top: 20px;
-
     .nav_img {
         width: 184px;
-
         img {
             width: 71%;
         }
@@ -55,6 +54,9 @@ export default {
             padding-bottom: 16px;
             margin-top: 19px;
             letter-spacing: -0.4px;
+            @media #{$phone, $smallphone}{
+                border: none;
+            }
         }
 
         .nav_places {
@@ -62,6 +64,9 @@ export default {
             flex-direction: row;
             justify-content: flex-start;
             padding-top: 8px;
+            @media #{$phone, $smallphone}{
+                display: none;
+            }
             span {
                 padding: 5px 32px 5px 20px;
                 font-weight: 800;
@@ -69,11 +74,37 @@ export default {
                 font-size: 13px;
                 text-transform: uppercase;
                 border-right: 1px solid $colorSilver;
+                @media #{$phone, $smallphone}{
+                    padding: 5px 0;
+                }
             }
             span:nth-child(1){
                 padding: 5px 22px 5px 0;
             }
+        }        
+    }    
+}
+
+.nav_places_mobile {
+    display: none;
+    @media #{$phone, $smallphone}{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        padding-top: 8px;
+        width: 100%;
+        border-top: 1px solid $colorSilver;
+    }
+    span{
+        @media #{$phone, $smallphone}{
+        padding: 5px;
+        color: $colorBlue;
+        font-size: 11px;
+        border-right: 1px solid $colorSilver;
         }
+    }
+    span:nth-child(4){
+        border: none;
     }
 }
 
@@ -85,6 +116,9 @@ export default {
     align-items: center;
     justify-content: center;
     width: 100%;
+    @media #{$phone, $smallphone}{
+        
+    }
     ul {
         display: flex;
         flex-direction: row;
@@ -97,9 +131,20 @@ export default {
         font-size: 12px;
         width: 76%;
         margin-bottom: 6px;
+        @media #{$phone, $smallphone}{            
+            font-size: 11px;
+            font-weight: 400;
+            text-transform: none;
+            padding: 2px 7px;
+            width: 100%;
+        }
+        
         li {
             padding: 25px 21px;
             letter-spacing: 1px;
+            @media #{$phone, $smallphone}{
+                padding: 2px;
+            }
         }
     }
 }
